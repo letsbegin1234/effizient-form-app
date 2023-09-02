58d8c01914ab8ce830796a6d32ebd216a2b33bf5
Subhash <98774745+letsbegin1234@users.noreply.github.com>
2023-09-02 17:13:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -325,5 +325,5 @@ app.post("/generate-sop", async (req, res) => {
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port xnx${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
